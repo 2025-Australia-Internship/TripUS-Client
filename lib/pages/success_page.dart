43e69fc51b824f461136df02267f101d0599f571
hayoung <jbg01200249@gmail.com>
2025-01-15 02:06:11 +1100

    feat : 로그인 & 회원가입 중간 완성

---
+++ lib/pages/success_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:tripus/colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tripus/pages/choose_page.dart';
+
+class SuccessPage extends StatefulWidget {
+  const SuccessPage({super.key});
+
+  @override
+  State<SuccessPage> createState() => _SuccessPageState();
+}
+
+class _SuccessPageState extends State<SuccessPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SvgPicture.asset(
+              'assets/Joining_Success.svg',
+              width: 300,
+            ),
+            SizedBox(height: 10),
+            SvgPicture.asset(
+              'assets/koala.svg',
+              width: 180,
+            ),
+            Spacer(),
+            SizedBox(
+              width: 315,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const ChoosePage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: MainColor,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Back',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
