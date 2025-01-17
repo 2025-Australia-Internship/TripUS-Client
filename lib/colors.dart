d26012b1890630035aff57caa7680187afa57ddc
hayoung <jbg01200249@gmail.com>
2025-01-18 00:18:56 +1100

    feat : AI landmark 인식 페이지

---
+++ lib/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const MainColor = Color(0xff0050FF);
+
+const light01 = Color(0xff2367FF);
+const light02 = Color(0xff3D7CFF);
+const light03 = Color(0xff558EFF);
+const light04 = Color(0xff6DA0FF);
+const light05 = Color(0xff85B0FF);
+const light06 = Color(0xff9DC1FF);
+const light07 = Color(0xffB5D1FF);
+const light08 = Color(0xffCDE0FF);
+const light09 = Color(0xffE6F0FF);
+
+const dark01 = Color(0xff0044DE);
+const dark02 = Color(0xff0039BE);
+const dark03 = Color(0xff002E9E);
+const dark04 = Color(0xff002480);
+const dark05 = Color(0xff001A63);
+const dark06 = Color(0xff001048);
+const dark07 = Color(0xff00072E);
+const dark08 = Color(0xff000216);
+
+const gray01 = Color(0xffF1F1F1);
+const gray02 = Color(0xffD9D9D9);
+const gray03 = Color(0xffC1C1C1);
+const gray04 = Color(0xffB1B1B1);
+const gray05 = Color(0xff5A5A5A);
---
+++ lib/pages/picture_AI.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:tripus/colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AIPage extends StatefulWidget {
+  const AIPage({super.key});
+
+  @override
+  State<AIPage> createState() => _AIPageState();
+}
+
+class _AIPageState extends State<AIPage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 110),
+            Text(
+              'Oops!\nThis picture is not OOO',
+              style: TextStyle(
+                fontFamily: 'Pretendard',
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: 260,
+              height: 310,
+              color: gray04,
+            ),
+            SizedBox(height: 80),
+            SizedBox(
+              width: 315,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: MainColor,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Take Photo Again',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            SizedBox(
+              width: 315,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: Color(0xffF1F1F1),
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Back',
+                  style: TextStyle(
+                    color: MainColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/home_icon.svg',
+              width: 24,
+              height: 24,
+              color:
+                  _selectedIndex == 0 ? Color(0xFF0050FF) : Color(0xFFD9D9D9),
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/map_icon.svg',
+              width: 24,
+              height: 24,
+              color:
+                  _selectedIndex == 1 ? Color(0xFF0050FF) : Color(0xFFD9D9D9),
+            ),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/polaroid_icon.svg',
+              width: 24,
+              height: 24,
+              color:
+                  _selectedIndex == 2 ? Color(0xFF0050FF) : Color(0xFFD9D9D9),
+            ),
+            label: 'Polaroid',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/myPage_icon.svg',
+              width: 24,
+              height: 24,
+              color:
+                  _selectedIndex == 3 ? Color(0xFF0050FF) : Color(0xFFD9D9D9),
+            ),
+            label: 'My page',
+          ),
+        ],
+      ),
+    );
+  }
+}
