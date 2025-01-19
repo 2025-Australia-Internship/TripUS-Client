ec9a1652d4325c61967c198ba4ba1f7feea7096e
hayoung <jbg01200249@gmail.com>
2025-01-19 23:21:58 +1100

    feat : polaroid 색상 변경 추가

--- lib/pages/edit_polaroid.dart
+++ lib/pages/edit_polaroid.dart
@@ -18,6 +18,8 @@ class _EditPolaroidState extends State<EditPolaroid> {
     });
   }
 
+  Color _color = Colors.white;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,11 +52,129 @@ class _EditPolaroidState extends State<EditPolaroid> {
             Container(
               width: 300,
               height: 460,
-              color: gray04,
+              decoration: BoxDecoration(
+                color: _color,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 5,
+                    spreadRadius: 3,
+                    offset: Offset(3, 5),
+                  ),
+                ],
+              ),
               child: Padding(
                 padding: EdgeInsets.fromLTRB(20, 30, 20, 100),
                 child: Container(
                   color: gray01,
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            SizedBox(
+              width: double.infinity,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (_color != Color(0xffFF6D6D)) {
+                            _color = Color(0xffFF6D6D);
+                          } else {
+                            _color = Colors.white;
+                          }
+                        });
+                      },
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xffFF6D6D),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (_color != Color(0xffFFCB71)) {
+                            _color = Color(0xffFFCB71);
+                          } else {
+                            _color = Colors.white;
+                          }
+                        });
+                      },
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xffFFCB71),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (_color != Color(0xffFDFF72)) {
+                            _color = Color(0xffFDFF72);
+                          } else {
+                            _color = Colors.white;
+                          }
+                        });
+                      },
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xffFDFF72),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (_color != Color(0xff9BFF76)) {
+                            _color = Color(0xff9BFF76);
+                          } else {
+                            _color = Colors.white;
+                          }
+                        });
+                      },
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xff9BFF76),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (_color != Color(0xff8DE8FF)) {
+                            _color = Color(0xff8DE8FF);
+                          } else {
+                            _color = Colors.white;
+                          }
+                        });
+                      },
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xff8DE8FF),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (_color != Color(0xffCE7BFF)) {
+                            _color = Color(0xffCE7BFF);
+                          } else {
+                            _color = Colors.white;
+                          }
+                        });
+                      },
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xffCE7BFF),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                  ],
                 ),
               ),
             ),
