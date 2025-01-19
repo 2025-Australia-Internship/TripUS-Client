5a10b20888ec1d027d25d4208eac62ceedf0f3a7
hayoung <jbg01200249@gmail.com>
2025-01-20 10:59:00 +1100

    feat : main 수정정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tripus/colors.dart';
-import 'package:tripus/pages/home_page.dart';
+import 'package:tripus/page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
