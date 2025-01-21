4c31a41a592f85f469c119cb1926414abf49849b
hayoung <jbg01200249@gmail.com>
2025-01-22 03:00:00 +1100

    [feat] : map 구현현

--- lib/pages/map/map_page.dart
+++ lib/pages/map/map_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:tripus/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
+import 'package:tripus/colors.dart';
 import 'package:tripus/pages/home/home_page.dart';
 import 'package:tripus/pages/polaroid/polaroid.dart';
 import 'package:tripus/pages/profile/profile.dart';
@@ -17,6 +19,18 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(-37.8136, 144.9631), // 멜버른
+          zoom: 20.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c'], // OSM 기본 타일
+          ),
+        ],
+      ),
       bottomNavigationBar: Container(
         width: double.infinity,
         height: 85,
