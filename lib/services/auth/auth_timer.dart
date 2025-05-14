526658eb7542c0c7f4e3f8c51017282793205b0c
hayeong120 <jbg01200249@gmail.com>
2025-05-14 19:22:34 +0900

    ✨[feat] : 타이머 기능

---
+++ lib/services/auth/auth_timer.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+class AuthTimer {
+  late int secondsRemaining;
+  Timer? _timer;
+  Function(int)? onTick;
+  Function? onTimeout;
+
+  AuthTimer({
+    this.onTick,
+    this.onTimeout,
+  });
+
+  // 타이머 감소
+  void start({int duration = 180}) {
+    secondsRemaining = duration;
+    _timer?.cancel(); // 기존 타이머 정지
+
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (secondsRemaining > 0) {
+        secondsRemaining--;
+        if (onTick != null) onTick!(secondsRemaining); // 시간표시를 갱신
+      } else {
+        timer.cancel();
+        if (onTimeout != null) onTimeout!();
+      }
+    });
+  }
+
+  void cancel() {
+    _timer?.cancel();
+  }
+
+  String formatTime() {
+    final minutes = (secondsRemaining ~/ 60).toString();
+    final sec = (secondsRemaining % 60).toString().padLeft(2, '0');
+    return '$minutes:$sec';
+  }
+
+  bool get isRunning => _timer?.isActive ?? false;
+}
