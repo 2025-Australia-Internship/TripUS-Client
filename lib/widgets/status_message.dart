b83dabb0e4c65a4cb912452dc08a7f0940cdd3b8
hayeong120 <jbg01200249@gmail.com>
2025-05-14 20:13:09 +0900

    ✨[feat] : 상태 메세지 컴포넌트

---
+++ lib/widgets/status_message.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:tripus/constants/colors.dart';
+
+enum MessageType { error, success }
+
+class StatusMessage extends StatelessWidget {
+  final String message;
+  final MessageType type;
+
+  const StatusMessage({
+    super.key,
+    required this.message,
+    required this.type,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Color color;
+    switch (type) {
+      case MessageType.error:
+        color = error;
+        break;
+      case MessageType.success:
+        color = success;
+        break;
+    }
+
+    return Text(
+      message,
+      style: TextStyle(
+        color: color,
+        fontSize: 11,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+}
